9e588679db8166cb3a6186f98d03c2dce7f867c3
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-18 20:44:23 -0500

    Set up Fat-Free; added htaccess file; added a header and title to the home page.

--- index.php
+++ index.php
@@ -5,3 +5,24 @@
  * Date: 2019-01-18
  * Time: 17:32
  */
+
+//turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once('vendor/autoload.php');
+
+//create and instance of the Base class
+$f3 = Base::instance();
+//turn on fat free error reporting
+$f3->set('DEBUG',3);
+
+//define a default route
+$f3->route('GET /', function(){
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
