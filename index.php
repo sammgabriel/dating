73a954caef83dc0a13285579cfacb7c8ad576a60
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-02-17 02:38:07 -0500

    Changed the site so that users who did not decide to get a premium account were not able to view the interests screen. Interests also did not show up on their summaries.

--- index.php
+++ index.php
@@ -47,7 +47,8 @@ $f3->route('GET|POST /personal-information',
     $f3->set('gender', array("Male", "Female"));
 
     // When the user clicks the submit button
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit']))
+    {
 
         // Post variables
         $firstName = $_POST['fname'];
@@ -63,15 +64,18 @@ $f3->route('GET|POST /personal-information',
         $f3->set('genderChoice', $_POST['gender']);
 
         // Checks if a first name was entered
-        if (isset($_POST['fname'])) {
+        if (isset($_POST['fname']))
+        {
 
             // validates first name
-            if (validName($firstName)) {
+            if (validName($firstName))
+            {
 
                 // creates a session variable if entry is valid
                 $_SESSION['fname'] = $firstName;
 
-            } else {
+            } else
+            {
 
                 // otherwise, displays an error
                 $f3->set("errors['fname']", "Please enter a valid first name");
@@ -79,15 +83,18 @@ $f3->route('GET|POST /personal-information',
         }
 
         // Checks if a last name was entered
-        if (isset($_POST['lname'])) {
+        if (isset($_POST['lname']))
+        {
 
             // validates last name
-            if (validName($lastName)) {
+            if (validName($lastName))
+            {
 
                 // creates a session variable if entry is valid
                 $_SESSION['lname'] = $lastName;
 
-            } else {
+            } else
+            {
 
                 // otherwise, displays an error
                 $f3->set("errors['lname']", "Please enter a last name");
@@ -95,15 +102,18 @@ $f3->route('GET|POST /personal-information',
         }
 
         // Checks if an age was entered
-        if (isset($_POST['age'])) {
+        if (isset($_POST['age']))
+        {
 
             // validates age
-            if (validAge($age)) {
+            if (validAge($age))
+            {
 
                 // creates a session variable if entry is valid
                 $_SESSION['age'] = $age;
 
-            } else {
+            } else
+            {
 
                 // otherwise, displays an error
                 $f3->set("errors['age']", "Please enter a valid age (Note: You must be 18 years old or older to use this website.)");
@@ -111,19 +121,28 @@ $f3->route('GET|POST /personal-information',
         }
 
         // Checks if a phone number was entered
-        if (isset($_POST['phone'])) {
+        if (isset($_POST['phone']))
+        {
 
             // validates phone number
-            if (validPhone($phone)) {
+            if (validPhone($phone))
+            {
 
                 // creates a session variable if entry is valid
                 $_SESSION['phone'] = $phone;
 
-            } else {
+            } else
+            {
 
                 // otherwise, displays an error
                 $f3->set("errors['phone']", "Please enter a valid phone number.");
             }
+        }
+
+        if (isset($_POST['premium']))
+        {
+
+            $_SESSION['premium'] = $_POST['premium'];
         }
 
         // Redirects the user to the next page if all entries are valid
@@ -209,13 +228,23 @@ $f3->route('GET|POST /profile',
     // Creates new session variables and redirects
     // the user to the next page when he or she clicks
     // the next button
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit']))
+    {
 
         $_SESSION['email'] = $_POST['email'];
         $_SESSION['state'] = $_POST['state'];
         $_SESSION['seeking'] = $_POST['seeking'];
         $_SESSION['bio'] = $_POST['bio'];
-        $f3->reroute('/interests');
+
+        if (isset($_SESSION['premium']))
+        {
+
+            $f3->reroute('/interests');
+        }
+        else
+        {
+            $f3->reroute('/summary');
+        }
     }
 
     $template = new Template();
@@ -252,20 +281,25 @@ $f3->route('GET|POST /interests',
         $activity = "";
 
         // when the user submits the form
-        if (isset($_POST['submit'])) {
+        if (isset($_POST['submit']))
+        {
 
             // if the user picks indoor interests
-            if (isset($_POST['indoor'])) {
+            if (isset($_POST['indoor']))
+            {
 
                 // validates the user's choices
-                foreach ($indoorHobbies as $hobby) {
+                foreach ($indoorHobbies as $hobby)
+                {
 
-                    if (validIndoor($hobby)) {
+                    if (validIndoor($hobby))
+                    {
 
                         $_SESSION['indoor'] = $_POST['indoor'];
                         //array_push($_SESSION['indoor'], $hobby);
 
-                    } else {
+                    } else
+                    {
 
                         // Otherwise, displays an error
                         $f3->set("errors['indoor']", "Please pick a valid indoor activity.");
@@ -275,17 +309,21 @@ $f3->route('GET|POST /interests',
             }
 
             // if the user picks outdoor interests
-            if (isset($_POST['outdoor'])) {
+            if (isset($_POST['outdoor']))
+            {
 
                 // validates the user's choices
-                foreach ($outdoorHobbies as $activity) {
+                foreach ($outdoorHobbies as $activity)
+                {
 
-                    if (validOutdoor($activity)) {
+                    if (validOutdoor($activity))
+                    {
 
                         $_SESSION['outdoor'] = $_POST['outdoor'];
                         //array_push($_SESSION['outdoor'], $hobby);
 
-                    } else {
+                    } else
+                    {
 
                         // Otherwise, displays an error
                         $f3->set("errors['outdoor']", "Please pick a valid outdoor activity.");
@@ -295,13 +333,15 @@ $f3->route('GET|POST /interests',
             }
 
             // if all entries are valid, redirect the user to the next page
-            if (validIndoor($hobby) && validOutdoor($activity)) {
+            if (validIndoor($hobby) && validOutdoor($activity))
+            {
 
                 $f3->reroute('/summary');
             }
 
             // if no interests were selected redirect the user to the next page
-            if (!isset($_POST['indoor']) && !isset($_POST['outdoor'])) {
+            if (!isset($_POST['indoor']) && !isset($_POST['outdoor']))
+            {
 
                 $f3->reroute('/summary');
             }
